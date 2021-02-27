d6f01149d775a248a599d61b3fc36fecd672699f
Michał Śledź <sledzson08@gmail.com>
2021-02-27 13:14:31 +0100

    Add simple TLS benchmark

--- tls/client.c
+++ tls/client.c
@@ -3,17 +3,24 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 #include <openssl/ssl.h>
+#include <openssl/bio.h>
+
+#include <time.h>
 
 int main(int argc, char **argv) {
 
-    if (argc != 3) {
-        printf("Pass remote ip and port\n");
+    if (argc != 4) {
+        printf("Pass remote ip, port and number of packets to encode\n");
         exit(EXIT_FAILURE);
     }
     
     char *remote_ip = argv[1];
     int remote_port = atoi(argv[2]);
+    int packets = atoi(argv[3]);
 
     printf("Remote addr: %s %d\n", remote_ip, remote_port);
     
@@ -55,6 +62,42 @@ int main(int argc, char **argv) {
         printf("TLS handshake error\n");
         exit(EXIT_FAILURE);
     }
+
+    char *msg = "GET / HTTP/1.1\r\n"
+        "Accept: text/html\r\n"
+        "Host: google.com\r\n"
+        "\r\n";
+    int size = strlen(msg);
+
+    int bytes = SSL_write(ssl, msg, size);
+    printf("Wrote %d to TLS connection\n", bytes);
+
+    char buf[1500];
+    bytes = SSL_read(ssl, buf, sizeof(buf));
+    printf("Read %d from TLS connection\n%s\n", bytes, buf);
+
+    // change bio
+    BIO *rbio = BIO_new(BIO_s_mem());
+    BIO *wbio = BIO_new(BIO_s_mem());
+    if (rbio == NULL || wbio == NULL) {
+        printf("Error initializing bio");
+        exit(EXIT_FAILURE);
+    }
+
+    SSL_set_bio(ssl, rbio, wbio);
+    sleep(1);
+
+    printf("Starting benchmark\n");
+    clock_t start = clock();
+    for(int i = 0; i < packets; i++) {
+        bytes = SSL_write(ssl, msg, size);
+        if (bytes != 55) {
+            printf("error\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    clock_t end = clock();
+    printf("Preaparing %d packets took: %f\n", packets, (float)(end-start) / CLOCKS_PER_SEC);
 
     return 0;
 }
