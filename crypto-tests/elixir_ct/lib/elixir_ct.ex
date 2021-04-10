64689ae23f8c4fadcf0029a3674beedb0c460193
Michał Śledź <sledzson08@gmail.com>
2021-04-10 14:52:31 +0200

    [crypto-tests] Add crypto example in Elixir

--- crypto-tests/elixir_ct/lib/elixir_ct.ex
+++ crypto-tests/elixir_ct/lib/elixir_ct.ex
@@ -1,18 +1,62 @@
 defmodule ElixirCt do
-  @moduledoc """
-  Documentation for `ElixirCt`.
-  """
+  use Bitwise
 
-  @doc """
-  Hello world.
+  @opaque t() :: %__MODULE__{
+            key: binary(),
+            hp_key: binary(),
+            nonce: binary(),
+            algorithm: :aes_gcm_128
+          }
 
-  ## Examples
+  defstruct key: <<>>,
+            hp_key: <<>>,
+            nonce: <<>>,
+            algorithm: :aes_gcm_128
 
-      iex> ElixirCt.hello()
-      :world
+  @spec init(binary(), binary(), binary()) :: ElixirCt.t()
+  def init(key, hp_key, algorithm) do
+    %__MODULE__{
+      key: key,
+      hp_key: hp_key,
+      # TODO check size of nonce
+      nonce: <<0::96>>,
+      algorithm: algorithm
+    }
+  end
 
-  """
-  def hello do
-    :world
+  def encrypt_pkt(state, header, payload) do
+    enc_payload = encrypt_payload(state, header, payload)
+    enc_header = encrypt_hdr(state, enc_payload, header)
+    enc_header <> enc_payload
+  end
+
+  def encrypt_hdr(state, payload, header) do
+    pn_len = 1
+    sample = binary_part(payload, 4 - pn_len, 16)
+    mask = new_mask(state.hp_key, sample)
+    rest_len = byte_size(header) - 1 - pn_len
+    <<first::8, rest::binary-size(rest_len), pn::binary-size(pn_len)>> = header
+    first = first ^^^ (:binary.at(mask, 0) &&& 0x1F)
+
+    pn =
+      for i <- 0..(pn_len - 1), into: <<>>, do: <<:binary.at(pn, i) ^^^ :binary.at(mask, i + 1)>>
+
+    <<first>> <> rest <> pn
+  end
+
+  def encrypt_payload(state, header, payload) do
+    {enc_payload, tag} =
+      :crypto.crypto_one_time_aead(state.algorithm, state.key, state.nonce, payload, header, true)
+
+    enc_payload <> tag
+  end
+
+  def generate_payload(size) do
+    for _i <- 0..size, into: <<>>, do: <<Enum.random(0..255)>>
+  end
+
+  def new_mask(hp_key, sample) do
+    :crypto.crypto_one_time(:aes_128_ecb, hp_key, sample, true)
+    |> binary_part(0, 5)
   end
 end
--- crypto-tests/elixir_ct/test/elixir_ct_test.exs
+++ crypto-tests/elixir_ct/test/elixir_ct_test.exs
@@ -1,8 +1,24 @@
 defmodule ElixirCtTest do
   use ExUnit.Case
-  doctest ElixirCt
 
-  test "greets the world" do
-    assert ElixirCt.hello() == :world
+  @header <<0x57, 0x25, 0xE7, 0x4F, 0x2D, 0x27, 0x5D, 0x12, 0x8B, 0x37, 0xB0, 0x47, 0x04, 0x16,
+            0x08, 0xA1, 0x84, 0x23, 0x65, 0xDB, 0xFA, 0xE7>>
+
+  @key <<10, 141, 102, 148, 37, 119, 128, 179, 47, 14, 68, 0, 205, 28, 26, 149>>
+
+  @hp_key <<0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D,
+            0x0E, 0x0F>>
+
+  test "encrypt header" do
+    payload = ElixirCt.generate_payload(100)
+    crypto_state = ElixirCt.init(@key, @hp_key, :aes_128_gcm)
+    enc_payload = ElixirCt.encrypt_payload(crypto_state, @header, payload)
+    ElixirCt.encrypt_hdr(crypto_state, enc_payload, @header)
+  end
+
+  test "encrypt payload" do
+    payload = ElixirCt.generate_payload(100)
+    crypto_state = ElixirCt.init(@key, @hp_key, :aes_128_gcm)
+    ElixirCt.encrypt_payload(crypto_state, @header, payload)
   end
 end
